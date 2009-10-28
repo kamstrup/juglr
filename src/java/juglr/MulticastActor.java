e75db52af2e30cf86fe1abf6c2f71ee39accea1f
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2009-10-28 11:59:46 +0100

    multicast and swarm actors
    Implement to new actors forwarding messages to other actors in different ways. The SwarmActor and MulticastActor

---
+++ src/java/juglr/MulticastActor.java
@@ -0,0 +1,112 @@
+package juglr;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Asynchronously forward incoming messages to a collection of delegates
+ * based on a given strategy.
+ */
+public class MulticastActor extends Actor {
+
+    public static interface Strategy {
+        Iterator<Address> recipients(Message msg);
+    }
+
+    private static class ForwardToAllStrategy implements Strategy {
+
+        private List<Address> delegates;
+
+        private ForwardToAllStrategy() {
+            delegates = new LinkedList<Address>();
+        }
+
+        public ForwardToAllStrategy(Iterable<Address> delegates) {
+            this.delegates = new LinkedList<Address>();
+            for (Address delegate : delegates) {
+                this.delegates.add(delegate);
+            }
+        }
+
+        public ForwardToAllStrategy(Address... delegates) {
+            this(Arrays.asList(delegates));
+        }
+
+        static ForwardToAllStrategy newForActors(Iterable<Actor> delegates) {
+            ForwardToAllStrategy self = new ForwardToAllStrategy();
+            for (Actor delegate : delegates) {
+                self.delegates.add(delegate.getAddress());
+            }
+            return self;
+        }
+
+        static ForwardToAllStrategy newForActors(Actor... delegates) {
+            return newForActors(Arrays.asList(delegates));
+        }
+
+        public Iterator<Address> recipients(Message msg) {
+            return delegates.iterator();
+        }
+    }
+
+    private Strategy strategy;
+
+    private MulticastActor() {
+
+    }
+
+    /**
+     * Create a new multicast actor forwarding all incoming messages to all
+     * members of {@code delegates}.
+     *
+     * @param delegates the collection of delegates to forward all messages to
+     */
+    public MulticastActor (Address... delegates) {
+        strategy = new ForwardToAllStrategy(delegates);
+    }
+
+    /**
+     * Create a new multicast actor forwarding all incoming messages to all
+     * members of {@code delegates}.
+     *
+     * @param delegates the collection of delegates to forward all messages to
+     */
+    public MulticastActor (Iterable<Address> delegates) {
+        strategy = new ForwardToAllStrategy(delegates);
+    }
+
+    /**
+     * Create a new multicast actor forwarding all incoming messages to all
+     * delegates determined by a given {@link Strategy}.
+     *
+     * @param strategy the {@link Strategy} used to determine the message
+     *                 recipients
+     */
+    public MulticastActor (Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public MulticastActor(Actor... delegates) {
+        strategy = ForwardToAllStrategy.newForActors(delegates);
+    }
+
+    public static MulticastActor newForActors(Iterable<Actor> delegates) {
+        MulticastActor self = new MulticastActor();
+        self.strategy = ForwardToAllStrategy.newForActors(delegates);
+        return self;
+    }
+
+    public static MulticastActor newForActors(Actor... delegates) {
+        return newForActors(Arrays.asList(delegates));
+    }
+
+    @Override
+    public void react(Message msg) {
+        Iterator<Address> recipients = strategy.recipients(msg);
+        while (recipients.hasNext()) {
+            send(msg, recipients.next());
+        }
+    }
+}
---
+++ src/java/juglr/SwarmActor.java
@@ -0,0 +1,115 @@
+package juglr;
+
+import java.util.*;
+
+/**
+ * Asynchronously forward messages to a set of delegates according to
+ * a given strategy. The SwarmActor will always forward a message to
+ * exactly one delegate. If you need to forward the same message to multiple
+ * delegates use a {@link MulticastActor}.
+ */
+public class SwarmActor extends Actor {
+
+    public static interface Strategy {
+
+        public Address next(Message msg);
+
+    }
+
+    private static class RoundRobinStrategy implements Strategy {
+        private Set<Address> delegates;
+        private Iterator<Address> iter;
+
+        private RoundRobinStrategy() {
+            delegates = new HashSet<Address>();
+        }
+
+         public RoundRobinStrategy (Iterable<Address> delegates) {
+            this.delegates = new HashSet<Address>();
+            for (Address delegate : delegates) {
+                this.delegates.add(delegate);
+            }
+        }
+
+        public RoundRobinStrategy (Address... delegates) {
+            this(Arrays.asList(delegates));
+        }
+
+        static RoundRobinStrategy newForActors(Iterable<Actor> delegates) {
+            RoundRobinStrategy self = new RoundRobinStrategy();
+            for (Actor delegate : delegates) {
+                self.delegates.add(delegate.getAddress());
+            }
+            return self;
+        }
+
+        static RoundRobinStrategy newForActors(Actor... delegates) {
+            return newForActors(Arrays.asList(delegates));
+        }
+
+        public Address next(Message msg) {
+            if (iter == null || !iter.hasNext()) {
+                iter = delegates.iterator();
+            }
+
+            return iter.next();
+        }
+    }
+
+    private Strategy strategy;
+
+    private SwarmActor() {
+
+    }
+
+    /**
+     * Create a new SwarmActor forwarding messages to {@code delegates}
+     * using a round-robin strategy
+     * @param delegates the collection of delegates to forward messages to
+     */
+    public SwarmActor(Address... delegates) {
+        strategy = new RoundRobinStrategy(delegates);
+    }
+
+    /**
+     * Create a new SwarmActor forwarding messages to {@code delegates}
+     * using a round-robin strategy
+     * @param delegates the collection of delegates to forward messages to
+     */
+    public SwarmActor(Iterable<Address> delegates) {
+        strategy = new RoundRobinStrategy(delegates);
+    }
+
+    public SwarmActor(Actor... delegates) {
+        strategy = RoundRobinStrategy.newForActors(delegates);
+    }
+
+    public static SwarmActor newForActors(Iterable<Actor> delegates) {
+        SwarmActor self = new SwarmActor();
+        self.strategy = RoundRobinStrategy.newForActors(delegates);
+        return self;
+    }
+
+    public static SwarmActor newForActors(Actor... delegates) {
+        return newForActors(Arrays.asList(delegates));
+    }
+
+    /**
+     * Create a new SwarmActor forwarding messages to {@code delegates}
+     * selected using the given {@link Strategy}.
+     * @param strategy the Strategy used for selecting the next address to
+     *                 forward an incoming message to
+     */
+    public SwarmActor(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public void react(Message msg) {
+        Address delegate = strategy.next(msg);
+
+        /* Send via the bus instead of this.send()
+         * to avoid rewriting the sender address */
+        getBus().send(msg, delegate);
+    }
+}
